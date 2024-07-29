4c75d7ae4df0d55831531d06bb831ade1a5021b2
JulioCDias <juliozulu2008@gmail.com>
2024-07-29 12:12:14 -0300

    Criação de um metodo com retorno e testes

--- src/Conta.java
+++ src/Conta.java
@@ -5,7 +5,19 @@ public class Conta {
     String titular;
     double saldo;
 
-    void depositar(double valor){ // Método de Classe, Void especifica que nao ha retorno
+    void depositar(double valor) { // Método de Classe, Void especifica que nao ha retorno
         saldo += valor;
     }
+
+    boolean sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Voçe relizou o saque de r$"+valor);
+            return true;
+        } else {
+            System.out.println("Voçe nao tem saldo suficiente para o Saque");
+            return false;
+        }
+    }
 }
+
--- src/TesteMetodo.java
+++ src/TesteMetodo.java
@@ -5,5 +5,8 @@ public class TesteMetodo {
       System.out.println("O Valor do saldo é: "+primeriaConta.saldo);
       primeriaConta.depositar(3000);
       System.out.println("O Valor do saldo é: "+primeriaConta.saldo);
+
+      primeriaConta.sacar(20000);
+        System.out.println(primeriaConta.saldo);
     }
 }
